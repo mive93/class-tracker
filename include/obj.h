95a9a3f226c7d71779e6036e1d29848bef93ab24
aartioli <241745@studenti.unimore.it>
2021-01-21 12:04:15 +0100

    added new fields to obj for cache support
    Signed-off-by: aartioli <241745@studenti.unimore.it>

--- include/obj.h
+++ include/obj.h
@@ -10,18 +10,18 @@
 namespace tracking{
 /*Object in meters*/
 struct obj_m{
-    float x     = 0;
-    float y     = 0;
-    int frame   = -1;
-    int cl      = -1;
-    int w       = 0;
-    int h       = 0;
-    float precision = 0.0;
-    double latitude = 0.0;
-    double longitude = 0.0;
+    float x         = 0;
+    float y         = 0;
+    int frame       = -1;
+    int cl          = -1;
+    int w           = 0;
+    int h           = 0;
+    float error     = 0.0;
+    float latitude  = 0.0;
+    float longitude = 0.0;
 
     obj_m(){}
-    obj_m(const float x_, const float y_, const int frame_, const int cl_, const int width, const int height, const float precision_) : x(x_), y(y_), frame(frame_), cl(cl_), w(width), h(height), precision(precision_){}
+    obj_m(const float x_, const float y_, const int frame_, const int cl_, const int width, const int height, const float error_, const float latitude_, const float longitude_) : x(x_), y(y_), frame(frame_), cl(cl_), w(width), h(height), error(error_), latitude(latitude_), longitude(longitude_){}
     void print();
 };
 }
